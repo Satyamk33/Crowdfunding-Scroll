8eda6cf96cdd7062b23d4547ac8134c2b6c43256
Satyam <145621166+Satyamk33@users.noreply.github.com>
2024-04-30 00:17:49 +0530

    Update CrowdFunding.sol

--- contracts/CrowdFunding.sol
+++ contracts/CrowdFunding.sol
@@ -13,60 +13,80 @@ contract CrowdFunding{
             uint256[] donations;
      }
 
+     struct Milestone {
+        string description;
+        uint256 targetAmount;
+        uint256 deadline;
+        bool completed;
+    }
+
      mapping(uint256 => Campaign) public campaigns;
-     mapping(address => Campaign[]) public myCampaigns;
+    mapping(uint256 => Milestone[]) public campaignMilestones;
 
-     uint256 public numberofCampaigns = 0;
+     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(string memory _title, string memory _description, uint256 _target, uint256 _deadline) public {
-            Campaign storage campaign = campaigns[numberofCampaigns];
+     function createCampaign( string memory _title, string memory _description, uint256 _target, uint256 _deadline ) public returns (uint256) {
+              Campaign storage campaign = campaigns[numberOfCampaigns];
+              require(campaign.deadline > block.timestamp, "The deadline should be a date in future");
+              campaign.owner = msg.sender;
+              campaign.title = _title;
+              campaign.description = _description;
+              campaign.target = _target;
+              campaign.deadline = _deadline;
+              campaign.amountCollected = 0;
+              numberOfCampaigns++;
+            return numberOfCampaigns - 1;
 
-            require(campaign.deadline > block.timestamp, "The deadline should be a date in future");
+     }
 
-            campaign.owner = msg.sender;
-            campaign.title = _title;
-            campaign.description = _description;
-            campaign.target = _target;
-            campaign.deadline = _deadline;
-            campaign.amountCollected = 0;
-
-            myCampaigns[msg.sender].push(campaign);
-
-            numberofCampaigns++;
+     function addMilestone(uint256 _campaignId, string memory _description, uint256 _targetAmount, uint256 _deadline) public {
+        require(_campaignId < numberOfCampaigns, "Campaign does not exist");
+        campaignMilestones[_campaignId].push(Milestone(_description, _targetAmount, _deadline, false));
     }
 
-    function updateCampaign(uint256 _campaignId, uint256 _newTarget, uint256 _newDeadline) public {
-    
-        Campaign storage campaign = campaigns[_campaignId];
-
-        require(msg.sender == campaign.owner, "Only the campaign owner can update the campaign");
-        require(_newDeadline > block.timestamp, "The deadline should be a date in future");
-
-        campaign.target = _newTarget;
-        campaign.deadline = _newDeadline;
-
-    }
-
-    function donateToCampaign(uint256 _id) public payable {
-        uint256 amount = msg.value;
-
+     function donateToCampaign(uint256 _id) public payable {
+        require(_id <= numberOfCampaigns, "Campaign does not exist");
         Campaign storage campaign = campaigns[_id];
-
+        require(campaign.deadline > block.timestamp, "Campaign deadline passed");
+        require(campaign.amountCollected + msg.value <= campaign.target, "Campaign target reached");
         campaign.donators.push(msg.sender);
-        campaign.donations.push(amount);
+        campaign.donations.push(msg.value);
+        campaign.amountCollected += msg.value;
+     }
 
-        campaign.amountCollected = campaign.amountCollected + amount;
-        
+     
+
+    function completeMilestone(uint256 _id, uint256 _milestoneIndex) public {
+        require(_id < numberOfCampaigns, "Campaign does not exist");
+        require(_milestoneIndex < campaignMilestones[_id].length, "Milestone does not exist");
+        Milestone storage milestone = campaignMilestones[_id][_milestoneIndex];
+        require(!milestone.completed, "Milestone already completed");
+        require(block.timestamp <= milestone.deadline, "Milestone deadline passed");
+        milestone.completed = true;
     }
 
-    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+    function releaseFunds(uint256 _id) public {
+        require(_id < numberOfCampaigns, "Campaign does not exist");
+        Campaign storage campaign = campaigns[_id];
+        require(msg.sender == campaign.owner, "Only campaign owner can release funds");
+        require(campaign.amountCollected >= campaign.target, "Target amount not reached");
+        Milestone[] storage milestones = campaignMilestones[_id];
+        for (uint256 i = 0; i < milestones.length; i++) {
+            require(milestones[i].completed, "All milestones must be completed");
+        }
+        payable(campaign.owner).transfer(campaign.amountCollected);
+        campaign.amountCollected = 0;
+    }
+
+
+       function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
         return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
-    function getCampaigns() public view returns (Campaign[] memory) {
-        Campaign[] memory allCampaigns = new Campaign[](numberofCampaigns);
+       function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
-        for(uint i =0; i < numberofCampaigns; i++ ){
+        for(uint i =0; i < numberOfCampaigns; i++ ){
             Campaign storage item = campaigns[i];
 
             allCampaigns[i] =  item;
